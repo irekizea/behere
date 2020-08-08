484ccfddced8def1019d06870e9b4a8587122bf0
DWL5 <ssop6403@gmail.com>
2020-08-08 14:48:29 +0900

    initial source commit

---
+++ app/src/main/java/com/behere/loc_based_reminder/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.behere.loc_based_reminder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
