290ae387cc2e8aad1f03760ae59928ad18815b5b
joo-chang <jooc031@naver.com>
2020-08-29 12:21:34 +0900

    Revert "no message"
    This reverts commit 0eb264e32677e21dc169ebf7b31cf71ebaa6f62e.

--- app/src/main/java/com/behere/loc_based_reminder/service/LocationUpdatingService.kt
+++ app/src/main/java/com/behere/loc_based_reminder/service/LocationUpdatingService.kt
@@ -17,7 +17,6 @@ import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import com.behere.loc_based_reminder.CommonApplication
 import com.behere.loc_based_reminder.ListActivity
-import com.behere.loc_based_reminder.MapActivity
 import com.behere.loc_based_reminder.R
 import com.behere.loc_based_reminder.data.response.Item
 import com.behere.loc_based_reminder.receiver.MyBroadcastReceiver
